0c804c79d7a2cdda850f3339ab2e05abb37ffa2a
Saikat Bhattacharjee <bhattacharjee.saikat3@gmail.com>
2025-12-31 07:57:20 +0530

    Exploring langsmith

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -30,7 +31,8 @@ Musk is a supporter of global far-right figures, causes, and political parties. 
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
